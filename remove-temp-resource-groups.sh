b48408a882974ef4f334a917229f9b51eb00f27a
Nick Ward <nickward@microsoft.com>
2017-12-18 04:57:31 +0000

    works on cloud shell

--- remove-temp-resource-groups.sh
+++ remove-temp-resource-groups.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-
+echo "Retrieving resource groups..."
 jsongroups=$(az group list --query "[?tags.Permanent!='True'].name")
 groups=$(echo $jsongroups | tr '"' '\n' | grep t)
+echo
 for group in ${groups}
 do
-	read -p "Remove resource group '$group'?" -n 1 -r
-	echo    # (optional) move to a new line
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-    	# do dangerous stuff
-		echo "Removing resource group '$group'..."
-		echo $group | xargs az group delete -y --no-wait -n
-	else
-		echo "Skipping resource group '$group'"
-	fi
+	read -p "Remove resource group '$group'?" answer
+	case $answer in
+   		[yY]* ) echo "Removing resource group '$group'..."
+			echo $group | xargs az group delete -y --no-wait -n
+			;;
+		* )
+			echo "Skipping resource group '$group'"
+			;;
+	esac
 	echo
 done
